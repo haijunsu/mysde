46310c4522f267682b994c1436ea44cca71c5295
navysu <navysu@1d54f8ac-0013-11de-bc25-5f29055a2a2d>
2009-03-04 16:47:05 +0000

    add remote debug options and jmx port

--- mysde/tomcat6/setEnv.bat
+++ mysde/tomcat6/setEnv.bat
@@ -1,5 +1,27 @@
 echo setting Env ...
 call ../setEnv.bat
+rem Define server ports
+set HTTP_PORT=9000
+set SSL_PORT=9001
+set JPDA_PORT=9002
+set JMX_PORT=9003
+set SERVER_PORT=9004
+set AJP_PORT=9005
+
+rem Define JDPA
+set JPDA_TRANSPORT=dt_socket
+set JPDA_ADDRESS=%JPDA_PORT%
+
 set CATALINA_HOME=%TOMCAT_HOME%
 set CATALINA_BASE=%cd%
 set SERVICE_NAME=HaijunTomcatService
+
+if not "%HAS_SET_CATALINA%" == "" goto end
+set CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote 
+set CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.port=%JMX_PORT%
+set CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.ssl=false 
+set CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.authenticate=false
+set HAS_SET_CATALINA=true
+
+:end
+
--- mysde/tomcat6/setconf.bat
+++ mysde/tomcat6/setconf.bat
@@ -1,11 +1,18 @@
 @echo off
-set HTTP_PORT=8080
-set SSL_PORT=8443
+call setEnv.bat
 md conf
+md webapps
+md webapps\manager
+md webapps\host-manager
+md webapps\ROOT
 copy /Y  %TOMCAT_HOME%\conf conf
+copy /Y tomcat-users.xml conf
 sed 's/@HTTP_PORT@/%HTTP_PORT%/g' server.xml.templete > server.tmp
+sed 's/@SERVER_PORT@/%SERVER_PORT%/g' server.tmp > server.tmp1
+sed 's/@AJP_PORT@/%AJP_PORT%/g' server.tmp1 > server.tmp
 sed 's/@SSL_PORT@/%SSL_PORT%/g' server.tmp > conf\server.xml
 del server.tmp*
+sed 's/ start / jpda start /g' %TOMCAT_HOME%\bin\startup.bat > tomcatDebug.bat
 rem echo ^<Context path="/" reloadable="true" docBase="%TOMCAT_HOME%\webapps\ROOT"/^> >conf\catalina\localhost\Root.xml
 rem echo ^<Context path="/manager" reloadable="true" docBase="%TOMCAT_HOME%\webapps\manager"/^> >conf\catalina\localhost\manager.xml
 rem echo ^<Context path="/host-manager" reloadable="true" docBase="%TOMCAT_HOME%\webapps\host-manager"/^> >conf\catalina\localhost\host-manager.xml
